d3234a93a416606c9f09bbfc432854b9b518f1ba
Doug Williams <similate@gmail.com>
2021-04-03 07:22:12 -0600

    Assignment to create UnitOfMesasure as part of ingredient.  Did a few things different that weren't part of the assignment.

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -9,8 +9,9 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-    // TODO
-    // private UnitOfMeasure uom;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -47,4 +48,11 @@ public class Ingredient {
         this.recipe = recipe;
     }
 
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
 }
